70d89a9258d0de4f437f04de69687da497ef918a
sadik <sadikkarki10@gmail.com>
2024-12-19 22:08:22 +0545

    Added Methods that interact with the database

--- src/components/CartPageComponents/CartComponent.js
+++ src/components/CartPageComponents/CartComponent.js
@@ -1,20 +1,54 @@
-import { UID } from "../Firebase/Auth";
-import { readData } from "../Firebase/CRUD";
-import { where } from "firebase/firestore";
+import { useState, useEffect } from "react";
+import { FetchCart } from "../Firebase/CRUD";
 import CartItemCard from "./CartItemCard";
 
 const CartComponent = () => {
-    const cartedBooks = readData("Carts").where("UID", "==", UID);
+    const [cartedBooks, setCartedBooks] = useState([]); 
+    const [loading, setLoading] = useState(true); 
+    const [error, setError] = useState(null);
 
-    return (
-        <div className="container mx-auto">
-            <h1 className="text-2xl font-bold text-center mt-10">Cart Page</h1>
-            {
-                cartedBooks.forEach(book => {
-                    <CartItemCard book={book} />
-                })
+    const TestUID = "sadik123456"
+
+    useEffect(() => {
+        const fetchCartData = async () => {
+            try {
+                const data = await FetchCart(TestUID);
+                setCartedBooks(data || []);
+            } catch (e) {
+                setError("Failed to fetch cart data");
+                console.error("Error FetchCart: ", e);
             }
-        </div>
+        };
+        fetchCartData();
+    }, []);
+
+    useEffect(() => {
+        if(cartedBooks.length > 0) setLoading(false);
+        console.log("Carted Books: ", cartedBooks);
+    }, [cartedBooks]);
+
+    if (loading) return <p>Loading...</p>;
+
+    if (error) return <p>Error: {error}</p>;
+
+    return(
+        <>
+        {cartedBooks.length > 0 ? (
+            <div className="container">
+                <div className="row">
+                    {cartedBooks.map((bookObj) => (
+                        <CartItemCard key={bookObj.id} book={bookObj} />
+                    ))}
+                </div>
+            </div>
+        ) : (
+            <div className="container">
+                <div className="row">
+                    <h2>Cart is Empty</h2>
+                </div>
+            </div>
+        )}
+        </>
     );
 };
 
--- src/components/CartPageComponents/CartItemCard.js
+++ src/components/CartPageComponents/CartItemCard.js
@@ -1,7 +1,16 @@
-const CartItemCard = ({ Book }) => {
+const CartItemCard = (Book) => {
+    const book = Book.book[0];
+    const qty = Book.book[1];
+    console.log(Book);
     return(
-        <div>
-            {}
+        <div className="h-20 w-full flex items-center justify-between border-b border-purple-200">
+            Book: { book.title }
+            <div className="flex items-center">
+                <div className="mr-2">Qty: { qty }</div>
+                <div className="mr-2">Price: ${ book.price }</div>
+                <div className="mr-2">Total: ${ }</div>
+                <button className="bg-red-500 text-white px-2 py-1 rounded">Remove</button>
+            </div>
         </div>
     );
 };
