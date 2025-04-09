be8b7360986c44fd5c9d8eb929076b914a751231
sadik <sadikkarki10@gmail.com>
2025-04-09 20:17:45 +0545

    firebase analytics

--- src/components/Firebase/Init.jsx
+++ src/components/Firebase/Init.jsx
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
 // TO Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,3 +20,4 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export const analytics = getAnalytics(app);
